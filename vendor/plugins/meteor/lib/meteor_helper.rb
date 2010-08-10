d5b31e673e290d3ec0246dfff2931b2034b8a4bb
Tyler Lesmann <redhatcat@gmail.com>
2010-08-10 10:07:42 -0600

    New meteor plugin

--- vendor/plugins/meteor/lib/meteor_helper.rb
+++ vendor/plugins/meteor/lib/meteor_helper.rb
@@ -2,5 +2,19 @@ module MeteorHelper
   def option_equal(val1,val2)
     val1.to_s == val2.to_s
   end
+
+  def replace_blank(s, substitute='-')
+    if not s.kind_of? String
+      s = s.to_s
+    end
+    if s.blank?
+      s = substitute
+    end
+    s
+  end
+
+  def meteor_includes
+    render :partial => 'meteor/includes'
+  end
 end
 
