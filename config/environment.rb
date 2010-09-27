e6232b043c351551112d4b5d1c2e4beedb0b7ca7
bretweinraub <bret@ckuru.com>
2010-09-27 12:58:32 +0000

    Moved dhtml menu into its own plugin.

--- config/environment.rb
+++ config/environment.rb
@@ -16,7 +16,7 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
+#    config.gem "hpricot", :version => '0.8.2', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
